9690f21ae86d407a072b806e880db1d86c2640bb
dakoo <hochul.shin0@gmail.com>
2016-07-13 13:59:18 +0900

    [SPA] Header의 Home에 highlight --> Category나 Menu를 선택하면 Menu를 Highlight

--- js/script.js
+++ js/script.js
@@ -45,6 +45,21 @@ var insertProperty = function (string, propName, propValue) {
   return string;
 }
 
+// Remove the class 'active' from home and switch to Menu button
+var switchMenuToActive = function () {
+  // Remove 'active' from home button
+  var classes = document.querySelector("#navHomeButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navHomeButton").className = classes;
+
+  // Add 'active' to menu button if not already there
+  classes = document.querySelector("#navMenuButton").className;
+  if (classes.indexOf("active") == -1) {
+    classes += " active";
+    document.querySelector("#navMenuButton").className = classes;
+  }
+};
+
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
@@ -89,6 +104,9 @@ function buildAndShowCategoriesHTML (categories) {
       $ajaxUtils.sendGetRequest(
         categoryHtml,
         function (categoryHtml) {
+          // Switch CSS class active to menu button
+          switchMenuToActive();
+
           var categoriesViewHtml =
             buildCategoriesViewHtml(categories,
                                     categoriesTitleHtml,
@@ -142,6 +160,9 @@ function buildAndShowMenuItemsHTML (categoryMenuItems) {
       $ajaxUtils.sendGetRequest(
         menuItemHtml,
         function (menuItemHtml) {
+          // Switch CSS class active to menu button
+          switchMenuToActive();
+
           var menuItemsViewHtml =
             buildMenuItemsViewHtml(categoryMenuItems,
                                    menuItemsTitleHtml,
